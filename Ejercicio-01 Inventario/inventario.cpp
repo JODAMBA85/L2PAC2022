2a06eca10feaa9ed5523c0e7425922ffc0e931a9
jonatan.amaya@unah.hn <mcruel31@gamil.com>
2022-05-30 23:36:46 -0600

    tarea1

--- Ejercicio-01 Inventario/inventario.cpp
+++ Ejercicio-01 Inventario/inventario.cpp
@@ -82,6 +82,42 @@ void salidaDeInventario() {
     movimientoInventario(codigo, cantidad, "-");
 }
 
+void ajustePositivoDeInventario() {
+    string codigo = "";
+    int cantidad = 0;
+
+    system("cls");
+    cout << endl;
+    cout << "Ajuste Positivo de Inventario" << endl;
+    cout << "*****************************" << endl;
+    cout << "Ingrese el codigo del producto: ";
+    cin >> codigo;
+    cout << endl;
+    cout << "Ingrese la cantidad del producto: ";
+    cin >> cantidad;
+    cout << endl;
+
+    movimientoInventario(codigo, cantidad, "+");
+}
+
+void ajusteNegativoDeInventario() {
+    string codigo = "";
+    int cantidad = 0;
+
+    system("cls");
+    cout << endl;
+    cout << "Ajuste Negativo de Inventario" << endl;
+    cout << "**********************************" << endl;
+    cout << "Ingrese el codigo del producto: ";
+    cin >> codigo;
+    cout << endl;
+    cout << "Ingrese la cantidad del producto: ";
+    cin >> cantidad;
+    cout << endl;
+
+    movimientoInventario(codigo, cantidad, "-");
+}
+
 int main(int argc, char const *argv[])
 {
     int opcion = 0;
@@ -89,14 +125,17 @@ int main(int argc, char const *argv[])
     while(true) {
         system("cls");
 
-        cout << "Sistema de Inventario"; 
+
+        cout << "Sistema de Inventario";
         cout << endl;
-        cout << "*********************";
+        cout << "***********************";
         cout << endl;
         cout << endl;
         cout << "1 - Productos" << endl;
         cout << "2 - Ingreso de Inventario" << endl;
         cout << "3 - Salida de Inventario" << endl;
+        cout << "4 - Ajuste Positivo" << endl;
+        cout << "5 - Ajuste Negativo" << endl;
         cout << "0 - Salir" << endl;
         cout << "Ingrese una opcion del menu: ";
         cin >> opcion;
@@ -112,7 +151,13 @@ int main(int argc, char const *argv[])
         case 3:
             salidaDeInventario();
             break;
-        
+        case 4:
+            ajustePositivoDeInventario();
+            break;
+        case 5:
+            ajusteNegativoDeInventario();
+            break;
+
         default:
             break;
         }
@@ -125,7 +170,7 @@ int main(int argc, char const *argv[])
         }
     }
 
- 
+
 
     return 0;
-}
+} 
